fda156f54766b4988ccdf2e376d1bd376c6342cb
JollyRoger4u <petterssonroger@hotmail.com>
2019-03-18 10:46:51 +0100

    Replaced header 	modified:   Header.php

--- Header.php
+++ Header.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-        <link rel="stylesheet" type="text/css" href="Nav.css">
+<head>
+  <script type="text/javascript" src="js.js"> </script>
+</head>
+
+  <html lang="en">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/Nav.css">
+    
+    <?php
+    require_once('functions.php');
+    require_once('classes.php');
+    ?>
+
     <header>
       <div class="navLeft">
           <a class="active" href="#store">Store</a>        
       </div>
-        <img src="logo.png">
-        <div class=navRight>
-        <a href="#logIn" class="right">LogIn</a>                          <!--To Be Hidden When Logged In -->
-        <a href="#register" class="right">Register</a>                    <!--To Be Hidden When Logged In -->
-        <a href="#profile" class="right">Profile</a>                      <!--To Be Hidden When NOT Logged In -->
-        <a href="#logOut" class="right">Log Out</a>                       <!--To Be Hidden When NOT Logged In -->
-        <a href="#shoppingCart" class="right">ShoppingCart</a>       
+        <img src="img/logo.png">
+        <div class=navRight>  
+        <a href="logIn.php" class="right">LogIn</a>                  <!--   To Be Hidden When Logged In      -->
+        <a href="logOut.php" class="right">Log Out</a>               <!--   To Be Hidden When NOT Logged In  -->
+        <a href="register.php" class="right">Register</a>            <!--   To Be Hidden When Logged In      -->
+        <a href="userprofile.php" class="right">Profile</a>          <!--   To Be Hidden When NOT Logged In  -->
+        <a href="varukorg.php" class="right">Le Grande Vino Tinto</a>       
       </div>
     </header>
 
